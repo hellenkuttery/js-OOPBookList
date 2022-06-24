a96cbf5ddcf343365456479e1ab6e45e5fd31541
hellenkuttery <helen@clarusway.com>
2022-06-24 13:41:36 +0300

    add validation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Book(title, author, isbn) {
 // To use with proto, we cretaed a UI object
 function UI() {}
 UI.prototype.addBook = function (book) {
-    console.log(this.book)
+  console.log(this.book);
   const list = document.getElementById("book-list");
   const row = document.createElement("tr");
 
@@ -18,20 +18,54 @@ UI.prototype.addBook = function (book) {
    <td> <a href="">X</a></td>
    `;
   list.appendChild(row);
+};
 
+UI.prototype.showMessage=function(message,className){
+  const div=document.createElement("div");
+ 
+  div.className=`alert ${className}`
+  const mymessage=document.createTextNode(message);
+  div.appendChild(mymessage);
+
+  const container=document.querySelector(".container");
+  const form=document.querySelector("#book-form");
+
+  container.insertBefore(div,form);
+
+setTimeout(function(){
+  document.querySelector(".alert").remove();
+},3000)
+}
+
+UI.prototype.clearFields = function (book) {
+  document.getElementById("title").value="";
+  document.getElementById("author").value="";
+  document.getElementById("isbn").value="";
+ 
 };
 
 document.getElementById("book-form").addEventListener("submit", function (e) {
   const mtitle = document.getElementById("title").value,
     mauthor = document.getElementById("author").value,
     misbn = document.getElementById("isbn").value;
-
-  const mybook = new Book(mtitle, mauthor, misbn);
-  console.log(mybook);
+    const mybook = new Book(mtitle, mauthor, misbn);
+    console.log(mybook);
+    const ui = new UI();
   
-  const ui=new UI();
+    if(mtitle===""|| mauthor==="" || misbn===""){
+     ui.showMessage("Please fill the fields","error");
+  
+     
 
-  ui.addBook(mybook);
+    }
+    else{
+      ui.addBook(mybook);
+      ui.clearFields();
+    }
+
+ 
+  
+
 
   e.preventDefault();
 });
